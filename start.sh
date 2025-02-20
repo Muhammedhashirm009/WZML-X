669da3544d13c266501cba68c0562d44384e7409
Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>
2025-02-20 23:30:35 +0530

    Update start.sh
    Signed-off-by: Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Run the bot first (update.py then bot) in the background
-python3 update.py && python3 -m bot &
+python3 app.py
 
 sleep 600
 
 # Start the Flask server
-python3 app.py
+
+python3 update.py && python3 -m bot &
